a2a3e3ed93021a132283541b8d1e62fbb89337b7
Shane Robertson <shanerob11062@gmail.com>
2023-05-04 10:16:44 +0100

    Minor Change

--- src/uk/ac/tees/cis2001/pocketbeasts/HeavyAttack.java
+++ src/uk/ac/tees/cis2001/pocketbeasts/HeavyAttack.java
@@ -16,7 +16,7 @@ public class HeavyAttack implements AttackInterface{
     public int performAttack(int availableMana) {
         if(availableMana >= 3) {
             this.attack = 3;
-            System.out.println("You attack with " + this.attack + " pts of damage");
+            // System.out.println("You attack with " + this.attack + " pts of damage");
         } else {
             // Not enough mana for this attack
             System.out.println("Not enough mana to attack ");
